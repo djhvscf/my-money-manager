20ddba2777a031b14ad10f55fbd1ef8d85efcaf1
djhvscf <dennishernandezvargas@gmail.com>
2020-04-19 02:09:02 -0600

    Initial commit

---
+++ MyMoneyManager/MyMoneyManager/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyMoneyManager.Models;
+
+namespace MyMoneyManager.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
